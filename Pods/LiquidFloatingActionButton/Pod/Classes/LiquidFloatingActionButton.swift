c01abd657e5497ec19be8193886e6681dbe53a36
Adam Moffitt <adammoffitt@Adams-Personal-Laptop.local>
2020-02-28 17:22:43 -0800

    renaming

--- Pods/LiquidFloatingActionButton/Pod/Classes/LiquidFloatingActionButton.swift
+++ Pods/LiquidFloatingActionButton/Pod/Classes/LiquidFloatingActionButton.swift
@@ -433,7 +433,7 @@ class CircleLiquidBaseView : ActionBarBaseView {
         return -1 * t * (t - 2)
     }
     
-    func didDisplayRefresh(_ displayLink: CADisplayLink) {
+    @objc func didDisplayRefresh(_ displayLink: CADisplayLink) {
         if opening {
             keyDuration += CGFloat(displayLink.duration)
             updateOpen()
